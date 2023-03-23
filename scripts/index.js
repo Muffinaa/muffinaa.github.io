91dee1fbfe4fd2e019d68f44618d3f8e9399b2b8
Muffinaa <oskarwojciakyt7@gmail.com>
2023-03-23 21:23:42 +0100

    stuff

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,13 @@
-// Create a realtime age that shows the time passed since I got born i.e 16.9323
-// years. The age should be updated every second.
-
-// Create a function that returns the age in years
 function getAge() {
-    // Get the current date
-    const now = new Date();
-    // Get the date of my birth
-    const birth = new Date(2006, 4, 18);
-    // Calculate the difference in milliseconds
-    const difference = now - birth;
-    // Convert it to years
-    const age = difference / 1000 / 60 / 60 / 24 / 365;
-    // Return it
-    return age;
-    }
-
-// Create a function that updates the age every second
+  const now = new Date();
+  const birth = new Date(2006, 4, 18);
+  const difference = now - birth;
+  const age = difference / 1000 / 60 / 60 / 24 / 365;
+  return age;
+}
 function updateAge() {
-    // Get the age element
-    const ageElement = document.getElementById('age');
-    // Update the age
-    ageElement.innerText = getAge().toFixed(6);
-    }
+  const ageElement = document.getElementById("age");
+  ageElement.innerText = getAge().toFixed(6);
+}
 
-// Update the age every second
 setInterval(updateAge, 1000);
-
-
-// Display my GitHub public repositories in a card
-// Username: Muffinaa
-
